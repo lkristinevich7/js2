f98de3d24be163f4bf2f9f8871298b405bc05527
Your Name <you@example.com>
2020-01-25 14:44:59 +0100

    Instrukcje warunkowe + kolejnosc wiazania wyrazen

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
-var product = "Książka JavaScript"
+var product = 'Książka JavaScript'
 var price = 97.99
-var tax = 23.00
+var tax = 23
 var promo = true
 var dateAdded = new Date(2020, 1, 20)
 
 // "Ksiazka JavaScript" - PROMOCJA - 97.99PLN (20.01.2020)
 
-console.log(dateAdded)
-
 // datetxt = dateAdded.getDate() + '.' + (dateAdded.getMonth() + 1) + '.' + dateAdded.getFullYear()
-datetxt = dateAdded.toLocaleDateString('pl')
+var datetxt = dateAdded.toLocaleDateString('pl')
 
-wynik = product + ' - ' + 'PROMOCJA - ' + price + 'PLN ' + '(' + datetxt + ') '
+var grossPrice = (price * (1 + tax / 100)).toFixed(2)
+
+var wynik = product + ' - ' + (promo ? 'PROMOCJA - ' : '') + grossPrice + ' PLN ' + '(' + datetxt + ') '
 
 console.log(wynik)
