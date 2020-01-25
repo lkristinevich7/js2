8cb59dc60d9c83306f73e1797a4194b25ad41173
Your Name <you@example.com>
2020-01-25 15:44:18 +0100

    While vs do while, loop with pagination example

---
+++ examples/while_vs_do_while.js
@@ -0,0 +1,17 @@
+var rand
+
+// doWhile ===============
+
+do {
+  rand = Math.random()
+  console.log(rand)
+} while (rand < 0.5)
+
+// while ===============
+
+rand = Math.random()
+
+while (rand < 0.5) {
+  rand = Math.random()
+  console.log(rand)
+}
--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
 
 var products = [
   {
-    name: 'Książka JavaScript',
-    price: 97.99,
-    tax: 23,
-    promo: true,
-    dataAdded: new Date(2020, 1, 20)
-  },
-  {
     name: 'Książka CSS',
     price: 65.99,
     tax: 23,
     promo: false,
     dataAdded: new Date(2020, 1, 15)
+  }, {
+    name: 'Książka JavaScript',
+    price: 97.99,
+    tax: 23,
+    promo: true,
+    dataAdded: new Date(2020, 1, 20)
+  }, {
+    name: 'Książka HTML',
+    price: 65.99,
+    tax: 23,
+    promo: false,
+    dataAdded: new Date(2020, 1, 15)
+  }, {
+    name: 'Książka React',
+    price: 97.99,
+    tax: 23,
+    promo: true,
+    dataAdded: new Date(2020, 1, 20)
   }
 ]
 
-var max = 5
-var i = 0
+var offset = 1
+var limit = 2
+// var max = products.length
+var max = offset + limit
+var i = offset
 
-while (i < max) {
-  //     i = i + 1
-  //     i += 1
-  //     ++i
-  //     i++
-  console.log('Product numer X', ++i)
+while (i < max && max <= products.length) {
+  var product = products[i]
+  var datetxt = product.dataAdded.toLocaleDateString('pl')
+  var grossPrice = (product.price * (1 + product.tax / 100)).toFixed(2)
+
+  // "Ksiazka JavaScript" - PROMOCJA - 97.99PLN (20.01.2020)
+  var wynik = product.name + ' - ' + (product.promo ? 'PROMOCJA - ' : '') + grossPrice + ' PLN ' + '(' + datetxt + ') '
+
+  console.log(wynik)
+
+  i++
 }
-
-var product = products[1]
-
-// "Ksiazka JavaScript" - PROMOCJA - 97.99PLN (20.01.2020)
-
-var datetxt = product.dataAdded.toLocaleDateString('pl')
-
-var grossPrice = (product.price * (1 + product.tax / 100)).toFixed(2)
-
-var wynik = product.name + ' - ' + (product.promo ? 'PROMOCJA - ' : '') + grossPrice + ' PLN ' + '(' + datetxt + ') '
-
-console.log(wynik)
