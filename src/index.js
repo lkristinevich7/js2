63b76621cfe0444b58ae709255b7802d5d0f8d44
Your Name <you@example.com>
2020-01-25 14:54:47 +0100

    Creating and accessing object properities

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-
-var product = 'Książka JavaScript'
-var price = 97.99
-var tax = 23
-var promo = true
-var dateAdded = new Date(2020, 1, 20)
+var product = {
+  name: 'Książka JavaScript',
+  price: 97.99,
+  tax: 23,
+  promo: true,
+  'data-added': new Date(2020, 1, 20)
+}
 
 // "Ksiazka JavaScript" - PROMOCJA - 97.99PLN (20.01.2020)
 
-// datetxt = dateAdded.getDate() + '.' + (dateAdded.getMonth() + 1) + '.' + dateAdded.getFullYear()
-var datetxt = dateAdded.toLocaleDateString('pl')
+var datetxt = product['data-added'].toLocaleDateString('pl')
 
-var grossPrice = (price * (1 + tax / 100)).toFixed(2)
+var grossPrice = (product.price * (1 + product.tax / 100)).toFixed(2)
 
-var wynik = product + ' - ' + (promo ? 'PROMOCJA - ' : '') + grossPrice + ' PLN ' + '(' + datetxt + ') '
+var wynik = name + ' - ' + (product.promo ? 'PROMOCJA - ' : '') + grossPrice + ' PLN ' + '(' + datetxt + ') '
 
 console.log(wynik)
