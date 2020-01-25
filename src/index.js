9fea278f0c846fed263930cd290f378940b9c1c4
Your Name <you@example.com>
2020-01-25 14:20:10 +0100

    Date functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 
-product = "Książka JavaScript"
-price = 97.99
-tax = 23.00
-promo = true
-dateAdded = new Date(2020, 0, 20)
-
+var product = "Książka JavaScript"
+var price = 97.99
+var tax = 23.00
+var promo = true
+var dateAdded = new Date(2020, 1, 20)
 
 // "Ksiazka JavaScript" - PROMOCJA - 97.99PLN (20.01.2020)
 
-datetxt = '?? 1231?'
+console.log(dateAdded)
+
+// datetxt = dateAdded.getDate() + '.' + (dateAdded.getMonth() + 1) + '.' + dateAdded.getFullYear()
+datetxt = dateAdded.toLocaleDateString('pl')
+
 wynik = product + ' - ' + 'PROMOCJA - ' + price + 'PLN ' + '(' + datetxt + ') '
 
 console.log(wynik)
